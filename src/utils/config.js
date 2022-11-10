3e10008c7ea4ea592c10ea05bf3b5a38830624eb
unknown <pvkhanhlk@gmail.com>
2022-11-10 23:39:28 +0700

    token_expire_1

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-export const TOKEN_CYBERSOFT ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAxNiIsIkhldEhhblN0cmluZyI6IjA4LzExLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2Nzg2NTYwMDAwMCIsIm5iZiI6MTYzNzY4NjgwMCwiZXhwIjoxNjY4MDEzMjAwfQ.QkTkDXeVpyqSwqxo_HmH-aQhbITi8vZC_UPJ7cPM3W4'
+export const TOKEN_CYBERSOFT ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udGVuZCA3MyIsIkhldEhhblN0cmluZyI6IjIzLzA1LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4NDgwMDAwMDAwMCIsIm5iZiI6MTY1OTg5MTYwMCwiZXhwIjoxNjg0OTQ3NjAwfQ.u471oZWr9EMgIb7oeyuaxfi8spgAgUuTkUHYSS9pBWg'
 export const DOMAIN = 'https://elearningnew.cybersoft.edu.vn/'
 export const ACCESSTOKEN = 'accessToken'
 export const USER_LOGIN = 'USER_LOGIN'
